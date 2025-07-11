b9789c509f86a51228cc02ce4ce96284f7c56188
guiKotoski <gui.kotoski@gmail.com>
2025-07-11 07:56:41 -0300

    concluindo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("1199999999", "Nokia 3310", "123456789", 64);
+        Smartphone iphone = new Iphone("1188888888", "iPhone 14", "987654321", 128);
+
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
